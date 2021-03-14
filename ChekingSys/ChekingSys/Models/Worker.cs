6e4d4b22548ee9f2d3342968828fb6b6b91eefad
Ivelints <44435923+Ivelints@users.noreply.github.com>
2021-03-14 13:51:09 +0200

    Not ready data base

---
+++ ChekingSys/ChekingSys/Models/Worker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChekingSys.Models
+{
+    public class Worker
+    {
+        [Key]
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
