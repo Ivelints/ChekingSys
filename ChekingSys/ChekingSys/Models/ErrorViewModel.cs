7972e67fbe1d6b5e79459d79109ee72d0e9c7d0e
Ivelints <44435923+Ivelints@users.noreply.github.com>
2021-03-14 12:32:43 +0200

    First

---
+++ ChekingSys/ChekingSys/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChekingSys.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
