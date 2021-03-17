c3dbbcdba6a3c6321d7d8a4c72c0bde1cd856a3a
Ivelints <44435923+Ivelints@users.noreply.github.com>
2021-03-17 18:59:21 +0200

    Problems

--- ChekingSys/ChekingSys/Models/Worker.cs
+++ ChekingSys/ChekingSys/Models/Worker.cs
@@ -10,10 +10,39 @@ namespace ChekingSys.Models
     {
         [Key]
         public int ID { get; set; }
-        public string Name { get; set; }
-        public string MidName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
+        public string NamеSurname  { get; set; }
         public string Position { get; set; }
+        public string March1 { get; set; }
+        public string March2 { get; set; }
+        public string March3 { get; set; }
+        public string March4 { get; set; }
+        public string March5 { get; set; }
+        public string March6 { get; set; }
+        public string March7 { get; set; }
+        public string March8 { get; set; }
+        public string March9 { get; set; }
+        public string March10 { get; set; }
+        public string March11 { get; set; }
+        public string March12 { get; set; }
+        public string March13 { get; set; }
+        public string March14 { get; set; }
+        public string March15 { get; set; }
+        public string March16 { get; set; }
+        public string March17 { get; set; }
+        public string March18 { get; set; }
+        public string March19 { get; set; }
+        public string March20 { get; set; }
+        public string March21 { get; set; }
+        public string March22 { get; set; }
+        public string March23 { get; set; }
+        public string March24 { get; set; }
+        public string March25 { get; set; }
+        public string March26 { get; set; }
+        public string March27 { get; set; }
+        public string March28 { get; set; }
+        public string March29 { get; set; }
+        public string March30 { get; set; }
+        public string March31 { get; set; }
+
     }
 }
